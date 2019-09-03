7aa919f9c50b667128b9743194c1214672114434
cflatminor <cflatminor92@gmail.com>
2019-09-03 22:49:32 +0300

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(request, response) {
+  response.send({a: 1});
+});
+
+app.listen(port, function () {
+  console.log ('we are on ' + port);
+});
